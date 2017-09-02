d93480a694005fdc300d0c000763b87caeb79d67
Ray Chang <raychang@pixnet.tw>
2017-09-03 00:12:28 +0800

    add Vuex default setting

--- src/main.js
+++ src/main.js
@@ -1,14 +1,28 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import Vuex from 'vuex';
 import navbar from './components/navbar';
 import router from './router';
 
+Vue.use(Vuex);
+
 Vue.config.productionTip = false;
+
+const store = new Vuex.Store({
+  state: {
+    username: 'ray20204',
+  },
+  mutations: {
+  },
+  actions: {
+  },
+});
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { navbar },
 });
