ab3031ff6bd46740ef4fe289781eed352e4047ce
Ray Chang <raychang@pixnet.tw>
2017-09-03 20:48:06 +0800

    調整文章列表版面增加bloggerInfo

--- src/components/article-list.vue
+++ src/components/article-list.vue
@@ -1,17 +1,28 @@
 <template>
-  <div class="article-list" style="padding-top: 5rem;">
-      <ul>
-          <li v-for="article in articles">{{ article.title  }}</li>
-      </ul>
+  <div class="article-list container" style="padding-top: 5rem;">
+      <div class="row">
+          <div class="col-2">
+              <bloggerInfo></bloggerInfo>
+          </div>
+          <div class="col-10">
+              <ul>
+                  <li v-for="article in articles">{{ article.title  }}</li>
+              </ul>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
+import bloggerInfo from './blogger-info.vue';
 export default {
   name: 'articleList',
   data() {
     return {
     };
+  },
+  components: {
+    bloggerInfo,
   },
   computed: {
       articles () {
