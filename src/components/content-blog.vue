a2feb227a52c3bae9a3b78f7c1942f037425f58c
Ray Chang <raychang@pixnet.tw>
2017-09-03 21:24:04 +0800

    增加變身input 觸發 action

--- src/components/content-blog.vue
+++ src/components/content-blog.vue
@@ -1,9 +1,21 @@
 <template>
-  <div class="container pt-5">
-       <div class="col-2">
-           <bloggerInfo></bloggerInfo>
-      </div>
-  </div>
+    <div class="blog-content container" style="padding-top: 5rem;">
+        <div class="row">
+            <div class="col-2">
+                <bloggerInfo></bloggerInfo>
+            </div>
+            <div class="col-10">
+                <form class="sudo-form">
+                    <div class="input-group">
+                        <input class="form-control" v-model="userName" type="text" placeholder="帳號">
+                        <span class="input-group-btn">
+                            <button class="btn btn-secondary" v-on:click.prevent="sudo">變身</button>
+                        </span>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -15,7 +27,18 @@ export default {
   },
   data() {
     return {
+      userName: '',
     };
   },
+  methods: {
+    sudo: function(e) {
+        if (0 === this.userName.length) {
+            alert('請填入帳號');
+            return false;
+        }
+
+        this.$store.dispatch('getUserInfo', this.userName);
+    }
+  }
 };
 </script>
